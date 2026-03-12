a400bb3f9370567edf08342cf2f73d113b4ca8e6
Mehdi Talalha <m.talalha@gmail.com>
2026-03-12 17:06:33 +0000

    Done Basic show name

--- BSHowName/Name.py
+++ BSHowName/Name.py
@@ -1,10 +1,24 @@
-import random, time, sys
+import random, time, sys, string
 
-
+strings = list(char for char in string.printable if char not in set(string.whitespace) and char not in set(string.punctuation))
 
 try:
+    # get the user name
     name = input("Enter Your Name: ")
-    pass
+    # check if the name is valide
+    if name == '':
+        print("Invalide name")
+        sys.exit()
+
+    memory = ''
+
+    for i in name:
+        char = None
+        while char != i:
+            char = random.choice(strings)
+            print(memory + char) 
+            time.sleep(0.1) 
+        memory = memory + i
 
 except KeyboardInterrupt:
     sys.exit()
